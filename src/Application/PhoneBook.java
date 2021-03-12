4a08b77ea581641a93d29c01e34c0c6a1055737a
Stefanos Bozos <stefbozos@hotmail.com>
2021-03-12 21:01:51 +0200

    - Updated comments in app.java class - Implemented the methods for modifying a contact, removing a contact and searching for a contact. - Changed the input parameters in the methods deleteContact(), modifyContact(), searchContact() and indexSearch() into String type.

--- src/Application/PhoneBook.java
+++ src/Application/PhoneBook.java
@@ -17,11 +17,11 @@ public class PhoneBook {
     //Removes a certain element in the userPhoneBook ArrayList.
     //Calls indexSearch() to check if the element exist in the list
     //and removes it from the list.
-    public static void deleteContact(Contacts contact){
-        int index = indexSearch(contact);
+    public static void deleteContact(String item){
+        int index = indexSearch(item);
         if(index >= 0){
-            userPhoneBook.remove(contact);
-            System.out.println(contact.getName() + " has been deleted.");
+            userPhoneBook.remove(index);
+            System.out.println(userPhoneBook.get(index).getName() + " has been deleted.");
         }else{
             System.out.println("The contact you are trying to delete " +
                     "does not exist in the phonebook.");
@@ -32,7 +32,7 @@ public class PhoneBook {
     //element given by the user. Checks first if the element exists
     //using the indexSearch() method and sets the new element at the index
     //of the previous element
-    public static void modifyContact(Contacts oldContact, Contacts newContact){
+    public static void modifyContact(String oldContact, Contacts newContact){
         int index = indexSearch(oldContact);
         if(index >= 0){
             userPhoneBook.set(index,newContact);
@@ -45,10 +45,10 @@ public class PhoneBook {
     //Searches for an element at the userPhoneBook arraylist
     //given by the user. Call the method indexSearch() to
     //check if the element exist and prints out the element.
-    public static void searchContact(Contacts contact){
-        int index = indexSearch(contact);
+    public static void searchContact(String item){
+        int index = indexSearch(item);
         if(index>=0){
-            System.out.println("Contact " + userPhoneBook.get(index) + "\n");
+            System.out.println(userPhoneBook.get(index) + "\n");
         }else {
             System.out.println("Contact not found.");
         }
@@ -57,8 +57,14 @@ public class PhoneBook {
     //Method that searches for the index of an element in
     //the arraylist. If it exist returns the index else
     //returns -1.
-    private static int indexSearch(Contacts contact){
-        return userPhoneBook.indexOf(contact);
+    private static int indexSearch(String item){
+        for(int i=0; i<userPhoneBook.size(); i++){
+            if(item.equals(userPhoneBook.get(i).getName())
+            || item.equals(userPhoneBook.get(i).getPhoneNumber())){
+                return i;
+            }
+        }
+        return -1;
     }
 
     //Prints out the whole userPhoneBook arraylist.
--- src/Application/app.java
+++ src/Application/app.java
@@ -7,6 +7,7 @@ public class app {
     private static Scanner input = new Scanner(System.in);
     private static Contacts myContact;
 
+    //Prints out the menu of options the user can make.
     public static void menu(){
 
         boolean flag = true;
@@ -25,15 +26,15 @@ public class app {
                 case 1:
                     storeContact();
                     break;
-//                case 2:
-//                    modifyContact();
-//                    break;
-//                case 3:
-//                    removeContact();
-//                    break;
-//                case 4:
-//                    searchContact();
-//                    break;
+                case 2:
+                    modifyContact();
+                    break;
+                case 3:
+                    removeContact();
+                    break;
+                case 4:
+                    searchContact();
+                    break;
                 case 5:
                     showContacts();
                     break;
@@ -48,24 +49,47 @@ public class app {
     //Method to add a new contact in the phonebook
     //Calls the method addToList(Contacts contact) from the phonebook class
     private static void storeContact(){
-        System.out.println("Enter name: \r");
+        System.out.print("Enter name: ");
         String name = input.nextLine();
-        System.out.println("Enter phone number: \r");
+        System.out.print("Enter phone number: ");
         String phoneNumber = input.nextLine();
         myContact = new Contacts(name,phoneNumber);
         PhoneBook.addToList(myContact);
     }
 
-//    public static void searchContact(){
-//        System.out.println("Search for: \r");
-//        String name = input.nextLine();
-////        myContact = new Contacts();
-//        PhoneBook.searchContact();
-//    }
+    //Replaces an existing contact with a new one
+    //calls the method modifyContact in the PhoneBook class
+    private static void modifyContact(){
+        System.out.print("Enter name or number to modify: ");
+        String item = input.nextLine();
+        System.out.println("Enter name of the new contact: ");
+        String name = input.nextLine();
+        System.out.print("Enter phone number of the new contact: ");
+        String phoneNumber = input.nextLine();
+        myContact = new Contacts(name, phoneNumber);
+        PhoneBook.modifyContact(item,myContact);
+    }
+
+    //Removes an existing contact from the PhoneBook ArrayList
+    //calls deleteContact method from the PhoneBook class
+    private static void removeContact(){
+        System.out.print("Enter name or number to remove: ");
+        String item = input.nextLine();
+        PhoneBook.deleteContact(item);
+    }
+
+
+    //Searches for an existing contact either by name or by phone number.
+    //calls searchContact method from the PhoneBook class
+    private static void searchContact(){
+        System.out.print("Search for: ");
+        String item = input.nextLine();
+        PhoneBook.searchContact(item);
+    }
 
     //Method that calls the .getList() method from the phonebook class
     //to print the whole ArrayList
-    public static void showContacts(){
+    private static void showContacts(){
         PhoneBook.getList();
     }
 
